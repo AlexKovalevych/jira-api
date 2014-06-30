2213937f16439751b77bf07199d50f3389fff6b9
Alex Kovalevych <alexkovalevych@gmail.com>
2014-07-01 01:07:02 +0300

    complete issue client

--- src/Clients/IssueClient.php
+++ src/Clients/IssueClient.php
@@ -99,4 +99,176 @@ class IssueClient extends AbstractClient
     {
         return $this->postRequest(sprintf('issue/%s/transitions', $idOrKey), $data);
     }
+
+    /**
+     * Returns comments by issue id or key
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1143
+     *
+     * @param  integer|string $idOrKey
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function getComments($idOrKey)
+    {
+        return $this->getRequest(sprintf('issue/%s/comment', $idOrKey));
+    }
+
+    /**
+     * Creates a new comment by issue id or key
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1164
+     *
+     * @param  integer|string $idOrKey
+     * @param  array          $data
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function createComment($idOrKey, array $data)
+    {
+        return $this->postRequest(sprintf('issue/%s/comment', $idOrKey), $data);
+    }
+
+    /**
+     * Updates a comment by issue id or key, comment id with given data
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1221
+     *
+     * @param  integer|string $idOrKey
+     * @param  integer        $commentId
+     * @param  array          $data
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function updateComment($idOrKey, $commentId, array $data)
+    {
+        return $this->putRequest(sprintf('issue/%s/comment/%s', $idOrKey, $commentId), $data);
+    }
+
+    /**
+     * Deletes a comment by issue id or key, comment id
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1250
+     *
+     * @param  integer|string $idOrKey
+     * @param  integer        $commentId
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function deleteComment($idOrKey, $commentId)
+    {
+        return $this->deleteRequest(sprintf('issue/%s/comment/%s', $idOrKey, $commentId));
+    }
+
+    /**
+     * Returns full worklog for the issue by issue id or key
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1771
+     *
+     * @param  integer|string $idOrKey
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function getFullWorklog($idOrKey)
+    {
+        return $this->getRequest(sprintf('issue/%s/worklog', $idOrKey));
+    }
+
+    /**
+     * Creates worklog for the issue by issue id or key
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1788
+     *
+     * @param  integer|string $idOrKey
+     * @param  array          $data
+     * @param  string         $adjustEstimate ("new", "leave", "manual", "auto")
+     * @param  string         $newEstimate    (required for "new" adjustEstimate)
+     * @param  string         $reduceBy       (required for "manual" adjustEstimate)
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function createWorklog($idOrKey, array $data, $adjustEstimate = null, $newEstimate = null, $reduceBy = null)
+    {
+        $parameters = http_build_query(array(
+            'adjustEstimate' => $adjustEstimate,
+            'newEstimate'    => $newEstimate,
+            'reduceBy'       => $reduceBy,
+        ));
+
+        if ($parameters) {
+            $parameters = sprintf('?%s', $parameters);
+        }
+
+        return $this->postRequest(sprintf('issue/%s/worklog%s', $idOrKey, $parameters), $data);
+    }
+
+    /**
+     * Returns worklog for the issue by issue id or key and worklog id
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1826
+     *
+     * @param  integer|string $idOrKey
+     * @param  integer        $worklogId
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function getWorklog($idOrKey, $worklogId)
+    {
+        return $this->getRequest(sprintf('issue/%s/worklog/%s', $idOrKey, $worklogId));
+    }
+
+    /**
+     * Updates worklog for the issue by issue id or key and worklog id
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1843
+     *
+     * @param  integer|string $idOrKey
+     * @param  integer        $worklogId
+     * @param  array          $data
+     * @param  string         $adjustEstimate
+     * @param  string         $newEstimate
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function updateWorklog($idOrKey, $worklogId, array $data, $adjustEstimate = null, $newEstimate = null)
+    {
+        $parameters = http_build_query(array(
+            'adjustEstimate' => $adjustEstimate,
+            'newEstimate'    => $newEstimate,
+        ));
+
+        if ($parameters) {
+            $parameters = sprintf('?%s', $parameters);
+        }
+
+        return $this->putRequest(sprintf('issue/%s/worklog/%s%s', $idOrKey, $worklogId, $parameters), $data);
+    }
+
+    /**
+     * Deletes worklog for the issue by issue id or key and worklog id
+     *
+     * @link  https://docs.atlassian.com/jira/REST/latest/#d2e1878
+     *
+     * @param  integer|string $idOrKey
+     * @param  integer        $worklogId
+     * @param  string         $adjustEstimate
+     * @param  string         $newEstimate
+     * @param  string         $increaseBy
+     *
+     * @return GuzzleHttp\Message\Response
+     */
+    public function deleteWorklog($idOrKey, $worklogId, $adjustEstimate = null, $newEstimate = null, $increaseBy = null)
+    {
+        $parameters = http_build_query(array(
+            'adjustEstimate' => $adjustEstimate,
+            'newEstimate'    => $newEstimate,
+            'increaseBy'     => $increaseBy,
+        ));
+
+        if ($parameters) {
+            $parameters = sprintf('?%s', $parameters);
+        }
+
+        return $this->deleteRequest(sprintf('issue/%s/worklog/%s%s', $idOrKey, $worklogId, $parameters));
+    }
 }
