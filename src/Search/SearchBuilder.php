da4b4338e5d5cc057a5e55268cd8cf6a0aab6352
Alex Kovalevych <alexkovalevych@gmail.com>
2014-07-03 02:17:39 +0300

    add comma

--- src/Search/SearchBuilder.php
+++ src/Search/SearchBuilder.php
@@ -177,7 +177,7 @@ class SearchBuilder
             'maxResults'    => $this->limit,
             'validateQuery' => $this->validateQuery,
             'fields'        => $this->fields,
-            'expand'        => $this->expandFields
+            'expand'        => $this->expandFields,
         );
     }
 }
