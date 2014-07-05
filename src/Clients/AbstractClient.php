88d4824060ec3b3ea9146c1176501053de9d695c
Luis Cordova <cordoval@gmail.com>
2014-07-05 18:55:12 -0500

    license files :)

--- src/Clients/AbstractClient.php
+++ src/Clients/AbstractClient.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of Jira API package.
+ *
+ * (c) 2014 Alex Kovalevych <alexkovalevych@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace JiraApi\Clients;
 
 use GuzzleHttp\Client as GuzzleClient;
